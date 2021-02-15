c8a0dcf2beda903949d251701a49b244425a68e5
Terrence Chan <terrence_c@ymail.com>
2021-02-15 15:45:41 -0600

    completed

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,124 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import {
+  Box,
+  Container,
+  Text,
+  Wrap,
+  WrapItem,
+  Input,
+  IconButton,
+  InputRightElement,
+  InputGroup,
+  useToast,
+} from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { getCuratedProps, getQueryPhotos } from '../lib/api';
 
-export default function Home() {
+export default function Home({ data }) {
+  const [photos, setPhotos] = useState(data);
+  const [query, setQuery] = useState('');
+  const toast = useToast();
+
+  const handleSubmit = async (e) => {
+    await e.preventDefault();
+    if (query === '') {
+      toast({
+        title: 'Error.',
+        description: 'Empty Search',
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+        position: 'top',
+      });
+    } else {
+      const res = await getQueryPhotos(query);
+      await setPhotos(res);
+      await setQuery('');
+    }
+  };
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>TkhcHus Pexels</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      <Box overflow="hidden" bgGradient={[
+    "linear(to-tr, teal.300,yellow.400)",
+    "linear(to-t, blue.200, teal.500)",
+    "linear(to-b, orange.100, purple.300)",
+  ]} minH="100vh">
+        <Container>
+          <Text
+            bgGradient="linear(to-l, #7928CA,#FF0080)"
+            bgClip="text"
+            fontWeight="extrabold"
+            mb="1rem"
+            textAlign="center"
+            fontSize={['4xl', '4xl', '5xl', '5xl']}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            TkhcHus X Pexels
+          </Text>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          <form onSubmit={handleSubmit}>
+            <InputGroup pb="1rem">
+              <Input
+                placeholder="Search for Iceland"
+                variant="ghost"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+              <InputRightElement
+                children={
+                  <IconButton
+                    aria-label="Search"
+                    icon={<SearchIcon />}
+                    bg="blue.500"
+                    color="blue.50"
+                    onClick={handleSubmit}
+                  />
+                }
+              />
+            </InputGroup>
+          </form>
+        </Container>
+        <Wrap px="1rem" spacing={4} justify="center">
+          {photos.map((pic) => (
+            <WrapItem
+              key={pic.id}
+              boxShadow="base"
+              rounded="20px"
+              overflow="hidden"
+              bg="white"
+              lineHeight="0"
+              _hover={{
+                boxShadow: 'dark-lg',
+                transform: 'scale(0.98)',
+              }}
+            >
+              <Link href={`/photos/${pic.id}`}>
+                <a>
+                  <Image
+                    src={pic.src.landscape}
+                    alt={pic.url}
+                    width={800}
+                    height={400}
+                  />
+                </a>
+              </Link>
+            </WrapItem>
+          ))}
+        </Wrap>
+      </Box>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const data = await getCuratedProps();
+  return { props: { data } };
 }
